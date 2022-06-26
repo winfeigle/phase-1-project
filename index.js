4eb1345a4bad4154d9fc8f862dfb1c5b090711ad
Win Feigle <win.feigle@gmail.com>
2022-06-26 17:19:41 -0600

    Add fetch links to index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-// const card = document.getElementByClassName('card')
+const card = document.getElementByClassName('card')
 
-// fetch('')
+//State Flag Image
+fetch(`https://www.states101.com/img/flags/svg/${state}.svg`)
+
+//Median Household Income
+fetch(`https://datausa.io/api/data?measure=Household%20Income%20by%20Race,Household%20Income%20by%20Race%20Moe&Geography=${city}-${stateAbbreviation}:similar&year=latest`)
+
+//Median Property value
+fetch(`https://datausa.io/api/data?measure=Property%20Value&Geography=${city}-${stateAbbreviation}:parents&year=latest`)
+
 
