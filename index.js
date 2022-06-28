281e36bbcf85f52c253188912edec718859efc8e
Win Feigle <win.feigle@gmail.com>
2022-06-28 16:53:43 -0600

    Add PropertyValue and Population fetches

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const form = document.getElementById('search-form')
 const incomeRow = document.getElementById('income')
 
 
-// //State Flag Image
-// fetch(`https://www.states101.com/img/flags/svg/${state}.svg`)
-
-// //Median Household Income
-// fetch(`https://datausa.io/api/data?measure=Household%20Income%20by%20Race,Household%20Income%20by%20Race%20Moe&Geography=${city}-${stateAbbreviation}:similar&year=latest`)
-
-// //Median Property value
-// fetch(`https://datausa.io/api/data?measure=Property%20Value&Geography=${city}-${stateAbbreviation}:parents&year=latest`)
-
-
 //EVENT LISTENERS
 form.addEventListener('submit', handleSubmit)
 
@@ -25,6 +15,8 @@ function handleSubmit(e){
 
     console.log(location)
     getIncomeData(location)
+    getPropertyData(location)
+    getPopulation(location)
 
     form.reset()
 }
@@ -35,14 +27,25 @@ function updateIncome(cityObj){
         currency: 'USD',
       });
 
-      let incomeData = document.createElement('td')
+      let incomeData = document.querySelector('#income .data')
       incomeData.textContent = income
+}
 
-      incomeRow.appendChild(incomeData)
+function updateProperty(cityObj){
+    let propertyValue = cityObj.data.slice(-1)['0']['Property Value'].toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      });
 
-      //update Income on card
-      console.log(income)
-    
+      let propertyData = document.querySelector('#property .data')
+      propertyData.textContent = propertyValue
+}
+
+function updatePopulation(cityObj){
+    let population = cityObj.data.slice(-1)['0']['Population'].toLocaleString()
+
+    let populationData = document.querySelector('#population .data')
+    populationData.textContent = population
 }
 
 
@@ -56,9 +59,33 @@ function renderCard(location){
 
 //FETCH REQUESTS
 function getIncomeData(location){
-
     fetch(`https://datausa.io/api/data?measure=Household%20Income%20by%20Race,Household%20Income%20by%20Race%20Moe&Geography=${location}:similar&year=latest`)
         .then(res => res.json())
         .then(cityObj => updateIncome(cityObj))
 
 }
+
+function getPropertyData(location){
+    fetch(`https://datausa.io/api/data?measure=Property%20Value&Geography=${location}:parents&year=latest`)
+        .then(res => res.json())
+        .then(cityObj => updateProperty(cityObj))
+}
+
+function getPopulation(location){
+    fetch(`https://datausa.io/api/data?measure=Population&Geography=${location}:parents&year=latest`)
+        .then(res => res.json())
+        .then(cityObj => {
+            updatePopulation(cityObj)
+        })
+}
+
+
+
+// //State Flag Image
+// fetch(`https://www.states101.com/img/flags/svg/${state}.svg`)
+
+// //Median Household Income
+// fetch(`https://datausa.io/api/data?measure=Household%20Income%20by%20Race,Household%20Income%20by%20Race%20Moe&Geography=${city}-${stateAbbreviation}:similar&year=latest`)
+
+// //Median Property value
+// fetch(`https://datausa.io/api/data?measure=Property%20Value&Geography=${city}-${stateAbbreviation}:parents&year=latest`)
